a80423c1d7e0b8b9d5c90d79f077ad928e7c0097
atomic <mariocraft987dev+githubacc2@gmail.com>
2024-10-31 08:36:05 -0700

    dfgber

--- t/f.js
+++ t/f.js
@@ -7,17 +7,6 @@ $(function() {
     var term = new Terminal('#input-line .cmdline', '#container output');
     term.init();
     
-    // Update the clock every second
-    setInterval(function() {
-      function r(cls, deg) {
-        $('.' + cls).attr('transform', 'rotate('+ deg +' 50 50)')
-      }
-      var d = new Date()
-      r("sec", 6*d.getSeconds())  
-      r("min", 6*d.getMinutes())
-      r("hour", 30*(d.getHours()%12) + d.getMinutes()/2)
-    }, 1000);
-    
   });
   
   document.getElementById('input-line').addEventListener('keypress', function (e) {
@@ -35,8 +24,10 @@ $(function() {
         if (commandArrays[0] == "run") {
             if (commandArrays[2] == null) {
               document.getElementById("output").innerHTML += "<br/><error>Cannot run if no task selected</error>";
-            } else {
+            } else if (commandArrays[2] == "main.new.chicken") {
               document.getElementById("output").innerHTML += "<br/>Running task..";
+            } else if (commandArrays[2] == "main.exit") {
+              window.close();
             }
         }
       } else {
