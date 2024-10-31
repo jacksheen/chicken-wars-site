e11f400f01d4f99ef87fb575899ac18228e0e6e6
atomic <mariocraft987dev+githubacc2@gmail.com>
2024-10-31 11:01:57 -0700

    colrsss

--- t/f.js
+++ t/f.js
@@ -39,6 +39,20 @@ $(function() {
           document.body.style.backgroundColor = "blue";
         } else if (commandArrays[1] == 2) {
           document.body.style.backgroundColor = "red";
+        } else if (commandArrays[1] == 3) {
+          document.body.style.backgroundColor = "green";
+        } else if (commandArrays[1] == 4) {
+          document.body.style.backgroundColor = "yellow";
+        } else if (commandArrays[1] == 5) {
+          document.body.style.backgroundColor = "lime";
+        } else if (commandArrays[1] == 6) {
+          document.body.style.backgroundColor = "dodgerblue";
+        } else if (commandArrays[1] == 7) {
+          document.body.style.backgroundColor = "magenta";
+        } else if (commandArrays[1] == 8) {
+          document.body.style.backgroundColor = "purple";
+        } else if (commandArrays[1] == 9) {
+          document.body.style.backgroundColor = "white";
         } else {
           document.getElementById("output").innerHTML += "<br/><error>Invalid Color</error>";
         }
